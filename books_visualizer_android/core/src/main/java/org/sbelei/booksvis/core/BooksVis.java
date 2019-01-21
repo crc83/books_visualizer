a60ac26a74badec77846d6354075db297408bb2f
Serhii Belei <sergiy.beley@gmail.com>
2019-01-21 16:36:20 +0200

    Cyrilic printed out succesfully

--- books_visualizer_android/core/src/main/java/org/sbelei/booksvis/core/BooksVis.java
+++ books_visualizer_android/core/src/main/java/org/sbelei/booksvis/core/BooksVis.java
@@ -1,9 +1,18 @@
 package org.sbelei.booksvis.core;
 
 import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.TextureData;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.TextureData.TextureDataType;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeBitmapFontData;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 
@@ -12,12 +21,27 @@ public class BooksVis implements ApplicationListener {
     SpriteBatch batch;
     float elapsed;
     BitmapFont font;
+    private FreeTypeBitmapFontData font15;
+    private FreeTypeBitmapFontData font22;
 
     @Override
     public void create () {
         texture = new Texture(Gdx.files.internal("libgdx-logo.png"));
         batch = new SpriteBatch();
-        font = new BitmapFont();//Gdx.files.internal("Calibri.fnt"),Gdx.files.internal("Calibri.png"),false); //or use alex answer to use custom font
+
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(
+                Gdx.files.internal("arial.ttf"));
+
+        /*
+        font15 = generator.generateData(15);
+        font22 = generator.generateData(80);
+        generator.dispose();
+
+        Texture textureBackground = new Texture(Gdx.files.internal("background.png"));
+        TextureRegion region = new TextureRegion(textureBackground);
+        font = new BitmapFont(font22,region, true); //or use alex answer to use custom font
+        */
+        font = generator.generateFont(40, "àáâã´äåºæçè³¿éêëìíîïğñòóôõö÷øùüşÿÀÁÂÃ¥ÄÅªÆÇÈ²¯ÉÊËÌÍÎÏĞÑÒÓÔÕÖ×ØÙÜŞß!.?'\",", false);
     }
 
     @Override
@@ -32,7 +56,7 @@ public class BooksVis implements ApplicationListener {
         batch.begin();
         if (font != null) {
             //font.draw(batch, "Hello World!", 500, 500);
-            font.draw(batch, "Ïğèâ³ò ñâ³ò", 550, 550);
+            font.draw(batch, "Ïğèâ³ò ñâ³ò!!!", 550, 550);
         }
         batch.draw(texture, 100+100*(float)Math.cos(elapsed), 100+25*(float)Math.sin(elapsed));
         batch.end();
