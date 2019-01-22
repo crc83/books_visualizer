eaf7ef2d269eab2369a767df35afaaeb7b40fda3
Serhii Belei <sergiy.beley@gmail.com>
2019-01-22 22:20:06 +0200

    list of words ready

--- src/main/java/org/sbelei/booksvis/App.java
+++ src/main/java/org/sbelei/booksvis/App.java
@@ -2,14 +2,22 @@ package org.sbelei.booksvis;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
 
 import org.apache.pdfbox.cos.COSDocument;
 import org.apache.pdfbox.io.RandomAccessBufferedFileInputStream;
 import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.sbelei.booksvis.nlp.ResponseParser;
 import org.sbelei.booksvis.nlp.UdpipeFacade;
+import org.sbelei.booksvis.nlp.Word;
 import org.sbelei.booksvis.pdf.PdfboxFacade;
 
 /**
@@ -19,6 +27,7 @@ import org.sbelei.booksvis.pdf.PdfboxFacade;
 public class App
 {
     public static void main(String[] args) {
+/*
         PdfboxFacade pdfBox = new PdfboxFacade();
         String rawOutput = pdfBox.parsePdf( "./dll/Bogdanovych_Mat_2ukr_2017.pdf");
         rawOutput = rawOutput.replaceAll("\r\n\\d+\\s*\r\n", "");
@@ -39,11 +48,36 @@ public class App
 
         String processed = udpipe.process(rawOutput);
 
+        save("./out/processed.txt", processed);
+        System.out.print(processed);
+*/
+        String processed = readFile("./out/processed.txt", StandardCharsets.UTF_8);
+        ResponseParser parser = new ResponseParser();
+        Map<String, Word> model = parser.parseModel(processed);
+
+        String allWords ="";
+        for (String word :  model.keySet()) {
+            allWords += word +"\r\n";
+        }
+        save("./out/words.txt",allWords);
+      }
+
+    private static String readFile(String path, Charset charset) {
         try {
-            new PrintStream("./out/processed.txt").println(processed);
+            byte[] encoded = Files.readAllBytes(Paths.get(path));
+            return new String(encoded, charset);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    @SuppressWarnings("resource")
+    private static void save(String fileName, String processed) {
+        try {
+            new PrintStream(fileName).println(processed);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        System.out.print(processed);
-      }
+    }
 }
--- src/main/java/org/sbelei/booksvis/nlp/ResponseParser.java
+++ src/main/java/org/sbelei/booksvis/nlp/ResponseParser.java
@@ -14,10 +14,11 @@ public class ResponseParser {
         Map<String, Word> result = Pattern.compile("\r?\n")
                 .splitAsStream(conluuInput)
                 .filter(p -> !p.startsWith("#")) //ignore comments
+                .filter(p -> !p.isBlank())
                 .map(line ->
                     Pattern.compile("\t").splitAsStream(line)
                     .collect(toCollection(LinkedList<String>::new)))
-                .filter(list -> list.get(3).equals("NUM") || list.get(3).equals("NOUN"))
+                .filter(list -> /*list.get(3).equals("NUM") ||*/ list.get(3).equals("NOUN"))
                 .collect(toMap(t -> t.get(2), t -> new Word(t),(v1, v2)-> v2, LinkedHashMap<String, Word>::new));
 
         return result;
