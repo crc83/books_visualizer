1314539d731b7ac7cb6f8eaf0e40ba5f0dc77e0b
Serhii Belei <sergiy.beley@gmail.com>
2019-02-09 18:37:37 +0200

    refactored main. since we process book step by step

--- src/main/java/org/sbelei/booksvis/App.java
+++ src/main/java/org/sbelei/booksvis/App.java
@@ -1,5 +1,7 @@
 package org.sbelei.booksvis;
 
+import static java.util.stream.Collectors.toCollection;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -8,13 +10,17 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import org.apache.pdfbox.cos.COSDocument;
 import org.apache.pdfbox.io.RandomAccessBufferedFileInputStream;
 import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.sbelei.booksvis.download.ImageDownlader;
 import org.sbelei.booksvis.nlp.ResponseParser;
 import org.sbelei.booksvis.nlp.UdpipeFacade;
 import org.sbelei.booksvis.nlp.Word;
@@ -27,7 +33,12 @@ import org.sbelei.booksvis.pdf.PdfboxFacade;
 public class App
 {
     public static void main(String[] args) {
-/*
+//        readPdfAndParseItToText();
+//        readParsedTextAndFilterNouns();
+        readWordsFromFileGoogleImagesAndDownloadThem();
+      }
+
+    private static void readPdfAndParseItToText() {
         PdfboxFacade pdfBox = new PdfboxFacade();
         String rawOutput = pdfBox.parsePdf( "./dll/Bogdanovych_Mat_2ukr_2017.pdf");
         rawOutput = rawOutput.replaceAll("\r\n\\d+\\s*\r\n", "");
@@ -37,8 +48,6 @@ public class App
 
         try {
             new PrintStream("./out/raw_text.txt").println(rawOutput);
-//            rawOutput = rawOutput.replaceAll("\\d+[°*]?.\\s", "<TASK>");
-//            new PrintStream("./out/raw_text2.txt").println(rawOutput);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -50,7 +59,9 @@ public class App
 
         save("./out/processed.txt", processed);
         System.out.print(processed);
-*/
+    }
+
+    private static void readParsedTextAndFilterNouns() {
         String processed = readFile("./out/processed.txt", StandardCharsets.UTF_8);
         ResponseParser parser = new ResponseParser();
         Map<String, Word> model = parser.parseModel(processed);
@@ -60,7 +71,21 @@ public class App
             allWords += word +"\r\n";
         }
         save("./out/words.txt",allWords);
-      }
+    }
+
+    private static void readWordsFromFileGoogleImagesAndDownloadThem() {
+        String wordsEdited = readFile("./out/words_edited.txt", StandardCharsets.UTF_8);
+        LinkedList<LinkedList<String>> wordsEditedCollection = Pattern.compile("\r?\n").splitAsStream(wordsEdited)
+                .filter(p -> !p.startsWith("#")) // ignore comments
+                .filter(p -> !p.isBlank())
+                .map(line -> Pattern.compile("&").splitAsStream(line).collect(toCollection(LinkedList<String>::new)))
+                .collect(toCollection(LinkedList<LinkedList<String>>::new));
+
+        ImageDownlader downloader = new ImageDownlader();
+        for (LinkedList<String> wordsList : wordsEditedCollection) {
+            downloader.download(wordsList.getLast());
+        }
+    }
 
     private static String readFile(String path, Charset charset) {
         try {
