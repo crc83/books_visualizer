d808cce6da945dcec19df2f3f61f9511cb076226
Serhii Belei <serhii.belei@weather.com>
2019-08-25 11:53:32 +0300

    Add configuration. And removed logging.

---
+++ book_vis_batch/src/main/java/org/sbelei/booksvis/BookVisCoreConfiguration.java
@@ -0,0 +1,18 @@
+package org.sbelei.booksvis;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BookVisCoreConfiguration {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+}
